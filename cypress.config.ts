40c5f5cb41b6f25f21e4935d6e9cb030984a1445
nn1k4 <39860945+nn1k4@users.noreply.github.com>
2025-07-31 13:52:37 +0300

    Add Cypress config

---
+++ cypress.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    specPattern: "client/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "client/cypress/support/e2e.ts",
+    baseUrl: "http://localhost:5173",
+  },
+});
