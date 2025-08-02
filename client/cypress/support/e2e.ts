a67289b57dbe3359a0c87d995ab98287f3bf9be1
nn1k4 <39860945+nn1k4@users.noreply.github.com>
2025-08-02 19:49:36 +0300

    fix: buffer console logs for Cypress

--- client/cypress/support/e2e.ts
+++ client/cypress/support/e2e.ts
@@ -4,10 +4,28 @@ import "./commands";
 // Alternatively you can use CommonJS syntax:
 // require('./commands');
 
+declare global {
+  interface Window {
+    __browserLogs?: string[];
+  }
+}
+
 Cypress.on("window:before:load", win => {
+  const logs: string[] = [];
+  win.__browserLogs = logs;
   const originalLog = win.console.log;
   win.console.log = (...args) => {
+    logs.push(args.map(String).join(" "));
     originalLog(...args);
-    cy.task("logToFile", args.map(String).join(" ")).catch(() => {});
   };
+});
+
+afterEach(() => {
+  cy.window().then(win => {
+    const logs = win.__browserLogs || [];
+    if (logs.length) {
+      cy.task("logToFile", logs.join("\n")).catch(() => {});
+    }
+    win.__browserLogs = [];
+  });
 });
